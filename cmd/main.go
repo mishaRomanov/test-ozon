5533837cd1c71044f6f0cf30e1aed2e0e572785b
mishaRomanov <mishanyaromanov910@gmail.com>
2023-11-03 00:01:01 +0300

    update main.go. just started to write some stuff to test basic things

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+
+	"github.com/mishaRomanov/test-ozon/internal/handler"
+	"github.com/sirupsen/logrus"
+)
+
 func main() {
+	//создаем сервер
+	server := http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		Addr:         ":8080",
+	}
+
+	//делаем обработчики
+	router := mux.NewRouter()
+
+	router.HandleFunc("/link/{link}", handler.HandleGet)
+
+	router.HandleFunc("/link/test", handler.HandlePost)
+
+	//врубаем
+	logrus.Fatalf("%v", server.ListenAndServe())
 
 }
