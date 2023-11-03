f8856d087d11aae9b3c945ae0c07431c4c534749
mishaRomanov <mishanyaromanov910@gmail.com>
2023-11-03 16:56:41 +0300

    add shortener

--- internal/shortener/shorten.go
+++ internal/shortener/shorten.go
@@ -1,9 +1,20 @@
 package shorten
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"strings"
+)
 
 func makeAShortLink(url string) string {
 	shortUrl := "shorti.fy/"
 	sum := sha256.Sum256([]byte(url))
+	b64 := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s", sum[:10])))
+	shortUrl += b64[:10]
+
+	strings.Replace(shortUrl, "=", "", 5)
+	strings.Replace(shortUrl, "/", "_", 2)
+	return shortUrl
 
 }
