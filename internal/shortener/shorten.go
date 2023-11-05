0c9c2cc72b4d6b95780ee1666fdf9d13c6480324
mishaRomanov <mishanyaromanov910@gmail.com>
2023-11-05 20:14:52 +0300

    changed shortener a bit

--- internal/shortener/shorten.go
+++ internal/shortener/shorten.go
@@ -1,18 +1,19 @@
 package shorten
 
 import (
-	"crypto/sha256"
 	"encoding/base64"
-	"fmt"
-	"strings"
+	"github.com/google/uuid"
 )
 
+func cleanShortLink(link string) string {
+	//создаем закодированный в б64 uuid
+	res := base64.StdEncoding.EncodeToString([]byte(link[:20]))
+	return res[:10]
+}
+
 func MakeAShortLink(url string) string {
-	shortUrl := "shorti.fy/"
-	sum := sha256.Sum256([]byte(url))
-	b64 := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s", sum[:10])))
-	shortUrl += b64[:10]
-	shortUrl = strings.TrimRight(shortUrl, "=")
-	finalUrl := shortUrl[:len(shortUrl)/2] + "_" + shortUrl[len(shortUrl)/2+1:]
-	return finalUrl
+	//создаем новый рандомный uuid
+	new := uuid.New()
+	return cleanShortLink(new.String())
+
 }
