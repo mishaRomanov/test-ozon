62b8de246fe698b58b396c6403b2eb2fb94bb148
mishaRomanov <mishanyaromanov910@gmail.com>
2023-11-06 13:45:52 +0300

    added func to provide existing link check

--- internal/shortener/shorten.go
+++ internal/shortener/shorten.go
@@ -3,6 +3,9 @@ package shorten
 import (
 	"encoding/base64"
 	"github.com/google/uuid"
+	storage2 "github.com/mishaRomanov/test-ozon/internal/storage"
+	storage "github.com/mishaRomanov/test-ozon/internal/storage/cache"
+	"github.com/sirupsen/logrus"
 )
 
 func cleanShortLink(link string) string {
@@ -11,9 +14,15 @@ func cleanShortLink(link string) string {
 	return res[:10]
 }
 
-func MakeAShortLink(url string) string {
+func MakeAShortLink(url string, inmemory *storage.Cache) (string, error) {
+	//проверяем, есть ли такая ссылка в мапе
+	for key, link := range inmemory.Cache {
+		if link == url {
+			logrus.Errorf("An attempt to create a short link: %v: \n%s", storage2.ErrAlreadyExists, key)
+			return "", storage2.ErrAlreadyExists
+		}
+	}
 	//создаем новый рандомный uuid
 	new := uuid.New()
-	return cleanShortLink(new.String())
-
+	return cleanShortLink(new.String()), nil
 }
