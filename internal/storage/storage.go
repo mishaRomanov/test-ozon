1a27a14846443210348af36308b3879e0021a6c1
mishaRomanov <mishanyaromanov910@gmail.com>
2023-11-06 15:29:20 +0300

    update storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,9 +3,10 @@ package storage
 import "errors"
 
 var (
-	ErrNotFound   = errors.New("link not found")
-	ErrNotWritten = errors.New("could not write the data")
-	ErrEmptyInput = errors.New("data wasn't written, input empty")
+	ErrNotFound      = errors.New("link not found")
+	ErrNotWritten    = errors.New("could not write the data")
+	ErrEmptyInput    = errors.New("data wasn't written, input empty")
+	ErrAlreadyExists = errors.New("short link already exists")
 )
 
 // создаем интерфейс как для постгреса
