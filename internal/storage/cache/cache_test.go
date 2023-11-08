1eaf8f07da908a7602c843e4ab0902796f1eb8b6
mishaRomanov <mishanyaromanov910@gmail.com>
2023-11-08 14:06:57 +0300

    add cache test

--- internal/storage/cache/cache_test.go
+++ internal/storage/cache/cache_test.go
@@ -7,8 +7,25 @@ import (
 )
 
 func TestWriteValue(t *testing.T) {
-	testmap := NewCache()
-	testmap.WriteValue("a", "b")
-	assert.Equal(t, testmap.WriteValue("", ""), storage.ErrEmptyInput)
-	assert.Equal(t, testmap.WriteValue("a", "b"), storage.ErrAlreadyExists)
+	object := NewCache()
+	t.Run("Successfull writing", func(t *testing.T) {
+		assert.Equal(t, nil, object.WriteValue("short", "full"))
+	})
+	t.Run("Link already exists", func(t *testing.T) {
+		assert.Equal(t, storage.ErrAlreadyExists, object.WriteValue("short", "full"))
+	})
+
+	//tests for empty input
+	t.Run("Empty input: both", func(t *testing.T) {
+		err := object.WriteValue("", "")
+		assert.Equal(t, storage.ErrEmptyInput, err)
+	})
+	t.Run("Empty input: full link", func(t *testing.T) {
+		err := object.WriteValue("", "world")
+		assert.Equal(t, storage.ErrEmptyInput, err)
+	})
+	t.Run("Empty input: short link", func(t *testing.T) {
+		err := object.WriteValue("hello", "")
+		assert.Equal(t, storage.ErrEmptyInput, err)
+	})
 }
