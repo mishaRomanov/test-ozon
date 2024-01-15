540b7ee9d92c2d5c4173df41a32b73846923c957
mishaRomanov <mishanyaromanov910@gmail.com>
2024-01-15 22:40:41 +0300

    update config

--- config/config.go
+++ config/config.go
@@ -6,15 +6,17 @@ import (
 
 // seting up a config for custom values for postgres
 type Config struct {
+	Storage      string `env:"STORAGE"`
 	User         string `env:"USER"`
-	Adress       string `env:"ADDRESS"`
+	Address      string `env:"ADDRESS"`
+	Port         string `env:"PORT"`
 	DatabaseName string `env:"DB_NAME"`
 	Password     string `env:"PASSWORD"`
 }
 
 func InitConfig() (Config, error) {
 	var config Config
-	//viper sets config path
+	//here we parse all environmental variables into struct object
 	err := env.Parse(&config)
 	if err != nil {
 		return Config{}, err
