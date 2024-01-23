ec54f0d4ec778eb073ba45087892489c8edbf346
mishaRomanov <mishanyaromanov910@gmail.com>
2024-01-23 18:14:55 +0300

    updated the shortener algorithm

--- internal/shortener/shorten.go
+++ internal/shortener/shorten.go
@@ -1,44 +1,12 @@
 package shorten
 
 import (
-	"encoding/base64"
-	"github.com/google/uuid"
+	"math/rand"
+	"strings"
+
 	"github.com/mishaRomanov/test-ozon/internal/storage"
 	"github.com/sirupsen/logrus"
-	"math/rand"
-	"strconv"
-	"unicode"
 )
-
-// we use that func in case it has the same index as slash
-func generateNum(n int) int {
-	return rand.Intn(n)
-}
-
-// this func checks whether the link has numbers in it or not
-func cleanShortLink(link string) string {
-	slash := rand.Intn(len(link))
-	link = link[:slash] + "_" + link[slash+1:]
-	for {
-		for _, char := range link {
-			//if we have at least one num. return
-			if unicode.IsNumber(char) {
-				return link
-			}
-		}
-		//generating a random num
-		num := rand.Intn(len(link))
-		//checking if slash index is equal to num
-		if num == slash {
-			num = generateNum(len(link))
-			link = link[:num] + strconv.Itoa(num) + link[num+1:]
-			break
-		}
-		link = link[:num] + strconv.Itoa(num) + link[num+1:]
-		break
-	}
-	return link
-}
 
 func MakeAShortLink(url string, dataStorage storage.Storager) (string, error) {
 	//checking if we already have that url
@@ -51,12 +19,19 @@ func MakeAShortLink(url string, dataStorage storage.Storager) (string, error) {
 		logrus.Errorf("%v", err)
 		return "", err
 	}
-	//creating UUID
-	new := uuid.New()
-	//Encode uuid to base64
-	res := base64.StdEncoding.EncodeToString([]byte(new.String()[:20]))
-	//we shorten uuid and then use only 10 elems of string
-	//in order to get rid of b64 junk like "==="
-	res = res[:10]
-	return cleanShortLink(res), nil
+	//creating a string with all needed characters
+	var alphabet = "ynAJfoSgdXHB5VasEMtcbPCr1uNZ4LG723ehWkvwYR6KpxjTm8iQUFqz9D"
+
+	//let's write len of the string into a variable
+	var alphabetLen = len(alphabet)
+	//creating a builder for building a string
+	var builder strings.Builder
+	//writing "_" cause we need it
+	builder.WriteString("_")
+
+	for i := 0; i < 9; i++ {
+		builder.WriteString(string(alphabet[rand.Intn(alphabetLen)]))
+	}
+	return builder.String(), nil
+
 }
